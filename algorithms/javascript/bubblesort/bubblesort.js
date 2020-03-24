a2e87d520932daf2c29f8d7d5fe355be30549eaf
dmaynard24 <davemaynard24@gmail.com>
2020-03-24 15:35:33 -0400

    added bubblesort algorithm (first pass)

---
+++ algorithms/javascript/bubblesort/bubblesort.js
@@ -0,0 +1,30 @@
+// Bubblesort
+
+// One of the most naive implementations of a sorting algorithm. Every element is compared to each of the other elements and swapped if out of order. This is repeated until all elements are in the correct order.
+
+// Bubblesort is O(n^2) time complexity but O(1) space complexity.
+
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function bubbleSort(arr) {
+  let isSorted = false;
+  let lastUnsorted = arr.length - 1;
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 0; i < lastUnsorted; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        isSorted = false;
+      }
+    }
+    lastUnsorted--;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([1, 5, 4, 2]));
