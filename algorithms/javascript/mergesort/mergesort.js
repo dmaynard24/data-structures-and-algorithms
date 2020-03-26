199df277fc9a2135425fa53aa5694f3fe04c7e9f
Dave Maynard <davemaynard24@gmail.com>
2020-03-26 11:26:33 -0400

    implemented Mergesort in JS

---
+++ algorithms/javascript/mergesort/mergesort.js
@@ -0,0 +1,43 @@
+// Mergesort
+
+function mergeSort(arr) {
+  function mergeHalves(left, middle, right) {
+    let temp = [];
+    let leftStart = left;
+    let rightStart = middle + 1;
+    const size = right - left + 1;
+
+    while (leftStart <= middle && rightStart <= right) {
+      if (arr[leftStart] <= arr[rightStart]) {
+        temp.push(arr[leftStart]);
+        leftStart++;
+      } else {
+        temp.push(arr[rightStart]);
+        rightStart++;
+      }
+    }
+
+    if (leftStart <= middle) {
+      temp = [...temp, ...arr.slice(leftStart, middle + 1)];
+    } else {
+      temp = [...temp, ...arr.slice(rightStart, right + 1)];
+    }
+
+    arr.splice(left, size, ...temp);
+  }
+
+  function mergeSortRecursive(left, right) {
+    if (left < right) {
+      const middle = Math.floor((left + right) / 2);
+      mergeSortRecursive(left, middle);
+      mergeSortRecursive(middle + 1, right);
+      mergeHalves(left, middle, right);
+    }
+  }
+
+  mergeSortRecursive(0, arr.length - 1);
+
+  return arr;
+}
+
+module.exports = { mergeSort };
