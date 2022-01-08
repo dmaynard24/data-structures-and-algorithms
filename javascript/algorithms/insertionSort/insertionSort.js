a1f91bd272f9db77164a93c07695623462d0dc59
Dave Maynard <dmayna@amazon.com>
2022-01-08 17:10:53 -0500

    slightly optimized insertionSort using binary search to find the insertion point rather than pairwise swaps

--- javascript/algorithms/insertionSort/insertionSort.js
+++ javascript/algorithms/insertionSort/insertionSort.js
@@ -1,14 +1,20 @@
-const { swap } = require(`../../util/swap`);
-
 // Insertion Sort
 
 const insertionSort = (arr, isAscending = true) => {
-  for (let start = 1; start < arr.length; start++) {
-    let j = start;
-    while (j > 0 && arr[j - 1] > arr[j] === isAscending) {
-      swap(arr, j - 1, j);
-      j -= 1;
+  const binarySearch = (left, right, x) => {
+    if (left >= right) return left;
+    const mid = Math.floor((left + right) / 2);
+    const pivot = arr[mid];
+    if (isAscending ? x <= pivot : x >= pivot) {
+      return binarySearch(left, mid, x);
     }
+    return binarySearch(mid + 1, right, x);
+  };
+
+  for (let i = 1; i < arr.length; i++) {
+    if (isAscending ? arr[i - 1] <= arr[i] : arr[i - 1] >= arr[i]) continue;
+    const index = binarySearch(0, i - 1, arr[i]);
+    arr = [...arr.slice(0, index), ...arr.splice(i, 1), ...arr.slice(index)];
   }
 
   return arr;
--- javascript/algorithms/insertionSort/insertionSort.test.js
+++ javascript/algorithms/insertionSort/insertionSort.test.js
@@ -4,8 +4,17 @@ test(`sorting the array [1, 5, 5, 6, 9, 8, 3] in ascending order will return [1,
   const arr = [1, 5, 5, 6, 9, 8, 3];
   expect(insertionSort(arr)).toStrictEqual([1, 3, 5, 5, 6, 8, 9]);
 });
+test(`sorting the array [1, 5, 5, 6, 5.5, 9, 8, 3] in ascending order will return [1, 3, 5, 5, 5.5, 6, 8, 9]`, () => {
+  const arr = [1, 5, 5, 6, 5.5, 9, 8, 3];
+  expect(insertionSort(arr)).toStrictEqual([1, 3, 5, 5, 5.5, 6, 8, 9]);
+});
 
 test(`sorting the array [1, 5, 5, 6, 9, 8, 3] in descending order will return [9, 8, 6, 5, 5, 3, 1]`, () => {
   const arr = [1, 5, 5, 6, 9, 8, 3];
   expect(insertionSort(arr, false)).toStrictEqual([9, 8, 6, 5, 5, 3, 1]);
+});
+
+test(`sorting the array [1, 5, 5, 6, 5.5, 9, 8, 3] in descending order will return [9, 8, 6, 5.5, 5, 5, 3, 1]`, () => {
+  const arr = [1, 5, 5, 6, 5.5, 9, 8, 3];
+  expect(insertionSort(arr, false)).toStrictEqual([9, 8, 6, 5.5, 5, 5, 3, 1]);
 });
